d322f8d0e79d59871c1bc4f584a8de5118f5ff64
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-16 12:45:25 -0300

    Creando el Formulario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header'
+import Formulario from './components/Formulario'
 
 
 function App() {
@@ -8,6 +9,21 @@ function App() {
       <Header 
         titulo='Clima React App'
       />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario 
+                
+              />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }

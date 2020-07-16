9de833b09474eeade6f34b558bd0fefc14d400a0
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-16 19:13:09 -0300

    Validando el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -9,6 +9,8 @@ const Formulario = () => {
         pais: ''
     });
 
+    const [error, guardarError] = useState(false);
+
     // extraer ciudad y pais
     const { ciudad, pais } = busqueda;
 
@@ -21,8 +23,27 @@ const Formulario = () => {
         });
     } 
 
+    // Cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // Validar
+        if (ciudad.trim() === '' || pais.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // Pasarlo al componente principal
+
+    }
+
     return (  
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            { error ? <p className="red darken-4 error">Todos los campos son obligatorios</p> : null }
             <div className="input-field col s12">
                 <input 
                     type="text"
@@ -51,6 +72,14 @@ const Formulario = () => {
                     <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">Pais: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <input 
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
     );
